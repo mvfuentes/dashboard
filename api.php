af8bf7715a414c42a07b49425249e4fa25223520
Mauricio Fuentes <mvfuentes@gmail.com>
2025-10-28 13:41:25 -0600

    Característica: Cambia el formato de las unidades de medidas en api.php

--- api.php
+++ api.php
@@ -4,8 +4,8 @@
 header('Content-Type: application/json');
 
 function formatBytes($bytes, $precision = 2) {
-    $units = array('B', 'KB', 'MB', 'GB', 'TB');
-    $bytes = max($bytes, 0);
+	//$units = array('B', 'KB', 'MB', 'GB', 'TB');
+    $units = array('Bytes', 'KiloBytes', 'MegaBytes', 'GigaBytes', 'TeraBytes');  $bytes = max($bytes, 0);
     $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
     $pow = min($pow, count($units) - 1);
     $bytes /= (1 << (10 * $pow));
